5ecee43cd63ceb6876a91623cc9e630f0ef0d310
SandeepaDilakshana <sandeepadilakshana@gmail.com>
2025-01-15 22:32:06 +0530

    Add Profile component to display user information

---
+++ component/Profile.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+
+export default function Profile() {
+  return (
+    <View>
+      <Text>Name: </Text>
+      <Text>Age: </Text>
+      <Text>Course: </Text>
+      <Text>Gender: </Text>
+    </View>
+  )
+}
