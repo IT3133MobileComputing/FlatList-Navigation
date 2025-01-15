71df181417be5b971fbd999ea8fb76d6524fc92d
SandeepaDilakshana <sandeepadilakshana@gmail.com>
2025-01-16 00:43:18 +0530

    Add navigation functionality to AddStudent component for adding new students

--- component/AddStudent.js
+++ component/AddStudent.js
@@ -1,13 +1,21 @@
 import { Image, View } from 'react-native'
 import { useState } from 'react';
 import { Text, TextInput, Button } from 'react-native-paper';
+import { useNavigation } from '@react-navigation/native';
 
 export default function AddStudent() {
-    const [name, setName] = useState(students);
+    const navigation = useNavigation();
+    const [name, setName] = useState();
+    const newStudent={
+        name:name,
+        id:"451d",
+        age:15
+    }
     return (
         <View>
             <TextInput mode='outlined' label="name" value={name} onChangeText={setName}/>
-            <Button>ADD</Button>
+            <Button onPress={()=>{navigation.popTo("StudentList",{newStudent});
+        }}>ADD</Button>
         </View>
     )
 } 
