936b83c4ade602f51383836a274ac31a141345e4
SandeepaDilakshana <sandeepadilakshana@gmail.com>
2025-01-15 23:42:18 +0530

    Enhance Profile component to display student details and profile picture

--- component/Profile.js
+++ component/Profile.js
@@ -1,12 +1,14 @@
-import { View, Text } from 'react-native'
+import { Image, View, Text } from 'react-native'
 
-export default function Profile() {
+export default function Profile({route}) {
+    const {student} = route.params;
   return (
     <View>
-      <Text>Name: </Text>
-      <Text>Age: </Text>
-      <Text>Course: </Text>
-      <Text>Gender: </Text>
+      <Text>Name: {student.name}</Text>
+      <Text>Age: {student.age}</Text>
+      <Text>Course: {student.course_name}</Text>
+      <Text>Gender: {student.gender}</Text>
+      <Image source={student.profile_pic} />
     </View>
   )
-}
+} 
