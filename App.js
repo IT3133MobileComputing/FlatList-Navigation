89788858e2970e002ee5b780305e94e2bb2483a5
SandeepaDilakshana <sandeepadilakshana@gmail.com>
2025-01-16 00:42:39 +0530

    Add AddStudent screen to navigation stack

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Profile from './component/Profile';
+import AddStudent from './component/AddStudent';
 
 export default function App() {
   const Stack = createNativeStackNavigator();
@@ -13,6 +14,7 @@ export default function App() {
         <Stack.Navigator>
           <Stack.Screen name="StudentList" component={StudentList} />
           <Stack.Screen name="Profile" component={Profile} />
+          <Stack.Screen name="Add" component={AddStudent} />
         </Stack.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>
